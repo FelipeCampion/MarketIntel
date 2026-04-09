4d2655348939e16239b9c24fd6a5dd6b84de3da9
Campion <felipeccampion@gmail.com>
2026-04-09 19:08:20 -0300

    Update 06_Procedures_Triggers.sql

--- 06_Procedures_Triggers.sql
+++ 06_Procedures_Triggers.sql
@@ -1,19 +1,3 @@
-use db_marketintel_produtos;
-go
-
-create or alter trigger trg_verificar_estoque_minimo
-on produtos
-after update
-as
-begin
-    insert into produtos_em_falta (id_produto, quant_disponivel, id_fornecedor, custo_compra_unid)
-    select i.id_produto, i.quant_disponivel, i.id_fornecedor, i.custo_compra_unid
-    from inserted i
-    where i.quant_disponivel <= 5
-    and not exists (select 1 from produtos_em_falta where id_produto = i.id_produto);
-end;
-go
-
 use db_marketintel_vendas;
 go
 
@@ -21,30 +5,50 @@ create or alter procedure sp_registrar_venda
     @id_cliente bigint,
     @id_produto bigint,
     @qtd int,
-    @forma_pgto int
+    @forma_pgto int,
+    @num_parcelas int = 1
 as
 begin
     set nocount on;
     declare @preco decimal(10,2);
     declare @id_venda_gerada bigint;
-    
+    declare @valor_total decimal(10,2);
+    declare @i int = 1;
+
     select @preco = preco_produto from dim_produtos where id_produto = @id_produto;
+    set @valor_total = (@preco * @qtd);
 
     if @preco is not null
     begin
-        insert into vendas (id_cliente, id_forma_pagamento, status_venda, valor_venda_total)
-        values (@id_cliente, @forma_pgto, 'concluido', (@preco * @qtd));
+        begin transaction;
+        try
+            insert into vendas (id_cliente, id_forma_pagamento, status_venda, valor_venda_total, qtd_parcelas_escolhida)
+            values (@id_cliente, @forma_pgto, 'concluido', @valor_total, @num_parcelas);
 
-        set @id_venda_gerada = scope_identity();
+            set @id_venda_gerada = scope_identity();
 
-        insert into itens_venda (id_venda, id_produto, quantidade_itens, valor_unitario)
-        values (@id_venda_gerada, @id_produto, @qtd, @preco);
+            insert into itens_venda (id_venda, id_produto, quantidade_itens, valor_unitario)
+            values (@id_venda_gerada, @id_produto, @qtd, @preco);
 
-        update dim_produtos 
-        set quant_disponivel = quant_disponivel - @qtd
-        where id_produto = @id_produto;
-        
-        print 'venda realizada e estoque atualizado!';
+            declare @valor_parcela decimal(10,2) = @valor_total / @num_parcelas;
+
+            while @i <= @num_parcelas
+            begin
+                insert into contas_a_receber (id_venda, numero_parcela, valor_parcela, data_vencimento)
+                values (@id_venda_gerada, @i, @valor_parcela, dateadd(month, @i, getdate()));
+                set @i = @i + 1;
+            end
+
+            update dim_produtos 
+            set quant_disponivel = quant_disponivel - @qtd 
+            where id_produto = @id_produto;
+
+            commit transaction;
+            print 'venda e parcelas geradas com sucesso!';
+        catch
+            rollback transaction;
+            raiserror('erro ao processar venda a prazo.', 16, 1);
+        end catch
     end
     else
     begin
@@ -53,21 +57,36 @@ begin
 end;
 go
 
-use db_marketintel_pedidos;
-go
-
-create or alter trigger trg_calcular_lucro_pedido
-on itens_pedido_ecom
+create or alter trigger trg_calcular_lucro_venda
+on itens_venda
 after insert
 as
 begin
-    insert into pedidose_lucros (id_pedido, valor_lucro_pedido)
+    set nocount on;
+    insert into vendas_lucros (id_venda, valor_lucro_venda)
     select 
         i.id_venda, 
         sum(i.valor_subtotal - (p.custo_compra_unid * i.quantidade_itens))
     from inserted i
     inner join dim_produtos p on i.id_produto = p.id_produto
     group by i.id_venda;
+end;
+go
+
+use db_marketintel_produtos;
+go
+
+create or alter trigger trg_verificar_estoque_minimo
+on produtos
+after update
+as
+begin
+    set nocount on;
+    insert into produtos_em_falta (id_produto, quant_disponivel, id_fornecedor, custo_compra_unid)
+    select i.id_produto, i.quant_disponivel, i.id_fornecedor, i.custo_compra_unid
+    from inserted i
+    where i.quant_disponivel <= 5
+    and not exists (select 1 from produtos_em_falta where id_produto = i.id_produto);
 end;
 go
 
@@ -79,6 +98,7 @@ create or alter procedure sp_gerar_fechamento_mensal
     @ano int
 as
 begin
+    set nocount on;
     declare @faturamento_vendas decimal(15,2);
     declare @faturamento_pedidos decimal(15,2);
 
